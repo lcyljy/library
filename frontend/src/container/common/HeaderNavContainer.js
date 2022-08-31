20cad5a73288c940d4b9b27e2c6552a7762e3844
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-08-31 22:43:10 +0900

    header 컨테이너 생성

---
+++ frontend/src/container/common/HeaderNavContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../../components/common/Header";
+
+function HeaderNavContainer() {
+  return <Header></Header>;
+}
+
+export default HeaderNavContainer;
