f1e9ce3e0746d9e83b94de0ba404f18df410e9e1
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-08-31 22:37:53 +0900

    styled-reset 및 globalstyles 지정

---
+++ frontend/src/components/auth/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset"; // style-reset 패키지
+import "../../lib/styles/fonts/nanumsquare.css";
+
+const GlobalStyles = createGlobalStyle` 
+    ${reset}
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    body {
+        font-family: 'NanumSquare', sans-serif;
+        font-size: 14px;
+        color: black;
+    }
+`;
+
+export default GlobalStyles;
