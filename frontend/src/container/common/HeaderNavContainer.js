d420e450a0d2187d7ce7ce2d908a40ad3ac413d2
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-09-08 23:57:02 +0900

    api test

--- frontend/src/container/common/HeaderNavContainer.js
+++ frontend/src/container/common/HeaderNavContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/common/Header";
 import MenuBar from "../../components/menu/MenuBar";
+import Test from "./Test";
 function HeaderNavContainer() {
   return (
     <>
       <Header></Header>
       <MenuBar></MenuBar>
+      <Test></Test>
     </>
   );
 }
