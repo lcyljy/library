37ddfe0ef9d7247c1968c87407d3ee7d03c5a558
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-09-14 00:30:10 +0900

    (style) footer 위치 추가

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,12 +2,14 @@ import React from "react";
 import HeaderNavContainer from "../container/common/HeaderNavContainer";
 import SearchMain from "../components/common/SearchMain";
 import BookListContainer from "../container/common/BookListContainer";
+import Footer from "../components/common/Footer";
 function HomePage() {
   return (
     <>
       <HeaderNavContainer></HeaderNavContainer>
       <SearchMain></SearchMain>
       <BookListContainer></BookListContainer>
+      <Footer></Footer>
     </>
   );
 }
