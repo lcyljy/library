f6987c8375e2fade8c46218ed44473f500b4eebf
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-09-13 18:45:42 +0900

    test 경로 삭제

--- frontend/src/container/common/HeaderNavContainer.js
+++ frontend/src/container/common/HeaderNavContainer.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Header from "../../components/common/Header";
 import MenuBar from "../../components/menu/MenuBar";
-import Test from "./Test";
+
 function HeaderNavContainer() {
   return (
     <>
       <Header></Header>
       <MenuBar></MenuBar>
-      <Test></Test>
     </>
   );
 }
