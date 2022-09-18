e78270f23bde70af498ab909aba1b5f9b761176a
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-09-18 14:25:57 +0900

    (feat, refactor) 십진분류표에 따른 option 추가 외 리팩토링
    (feat) 십진분류표에 따른 option 추가
    (refactor) 사용하지 않는 주석 삭제
    
    소요시간 15분
    #5

--- frontend/src/components/fillter/BookFillter.js
+++ frontend/src/components/fillter/BookFillter.js
@@ -1,13 +1,11 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import palette from "../../lib/styles/palette";
 import Button from "../common/Button";
 import { LibraryList } from "../../lib/documents/LibraryList";
-import { useState } from "react";
-
+import { KDC } from "../../lib/documents/KDC";
 const FillterContainer = styled.div`
   width: 100%;
-  height: 80px;
   background-color: ${palette.gray[2]};
   display: flex;
   flex-direction: column;
@@ -59,6 +57,20 @@ const BookAll = styled(Button)`
   }
 `;
 
+const SubjectBook = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  height: 3.5rem;
+  background-color: ${palette.gray[2]};
+  padding: 0.8rem;
+`;
+const SubjectBookSelect = styled.select`
+  font-size: 1.2em;
+  width: 180px;
+  height: 2.2rem;
+  padding: 8px 5px;
+`;
 // 기본으로 가져온 도서관 리스트에서 '수원시립'삭제
 const LocOptions = Object.keys(LibraryList).map((el) =>
   el.replace(/수원시립/g, "")
@@ -70,11 +82,12 @@ const YearOptions = [18, 19, 20, 21, 22];
 const MonOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const GenreOptions = ["일반도서", "어린이도서", "주제별도서"];
 
+const KDCList = KDC.contents.categoryList;
+console.log(KDCList);
+
 function BookFillter(props) {
   const toggleSwitchHandler = (e) => {
     // props.setSelectedGenre(e => e.target.value)
-    // e.target.value에 따라 해당 값의 index를 연동시켜보고 싶었는데 실패.
-    // 하드코딩한면이 없잖아 있어서 나중에 리팩토링 필요
     GenreOptions.map((v, i) =>
       e.target.value === v ? props.setSelectedGenreIndex(i) : null
     );
@@ -124,8 +137,20 @@ function BookFillter(props) {
           </BookAll>
         ))}
       </BookAllWrap>
+      {props.selectedGenre === GenreOptions[2] ? (
+        <SubjectBook>
+          <SubjectBookSelect>
+            <option defaultValue='전체' value='전체' key={`all`}>
+              전체
+            </option>
+            {KDCList?.map((v, i) => (
+              <option key={i}>{v.keyword}</option>
+            ))}
+          </SubjectBookSelect>
+        </SubjectBook>
+      ) : null}
     </FillterContainer>
   );
 }
-// const [selectedGenre, setSelectedGenre] = useState("normalBook")
+
 export default BookFillter;
