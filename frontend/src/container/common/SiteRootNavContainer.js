1384b1f7ff972dd4dbc857bf8ee71f832a387d9b
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-09-19 12:19:10 +0900

    (feat) siteNav바를 가져올 컨테이너 생성
    siteNav바를 가져올 컨테이너 생성
    
    #8

---
+++ frontend/src/container/common/SiteRootNavContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+import SiteRootNav from "../../components/common/SiteRootNav";
+function SiteRootNavContainer() {
+  return <SiteRootNav></SiteRootNav>;
+}
+
+export default SiteRootNavContainer;
