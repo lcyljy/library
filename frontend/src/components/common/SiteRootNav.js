6b1367037cb332ae71cd68a5dab88665a8219eea
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-09-19 12:18:37 +0900

    (feat) site 상단 nav바 생성
     site 상단 nav바 생성
    
    #8

---
+++ frontend/src/components/common/SiteRootNav.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import bg_sub_title from "../../lib/img/main/bg_sub_title.jpg";
+
+const TitleSub = styled.div`
+  width: 100%;
+  background: url(${bg_sub_title});
+  background-size: 100%;
+  margin-bottom: 20px;
+  height: 85px;
+  line-height: 85px;
+  margin: 0 auto;
+  h2 {
+    width: 80%;
+    color: white;
+    font-size: 28px;
+    line-height: 85px;
+    margin: 0 auto;
+  }
+`;
+
+const Inner = styled.div`
+  position: relative;
+  width: 100%;
+  margin: 0 auto;
+  display: inline-block;
+`;
+function SiteRootNav() {
+  return (
+    <TitleSub>
+      <Inner>
+        <h2> 경로 </h2>
+        <ul>
+          <li></li>
+        </ul>
+      </Inner>
+    </TitleSub>
+  );
+}
+
+export default SiteRootNav;
