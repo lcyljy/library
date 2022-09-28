f8d1467c657d00cf584b3d3fa275db3adb3581b5
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-09-29 00:57:45 +0900

    (feat) 희망도서 신청 page 수정
    희망도서 신청 page 수정
    
    #13

--- frontend/src/components/etc/BaroLoan.js
+++ frontend/src/components/etc/BaroLoan.js
@@ -9,12 +9,35 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import icon_blue from "../../lib/img/nav_icon/icon-blue.png";
 import icon_tri from "../../lib/img/nav_icon/icon-tri.png";
+import arrow_select_other from "../../lib/img/nav_icon/arrow_select_other.png";
+import arrow_default_other from "../../lib/img/nav_icon/arrow_default_other.png";
+import BaroLoanServiceDoc from "./BaroLoanServiceDoc";
+import BaroLoanServiceDetailDoc from "./BaroLoanServiceDetailDoc";
+import LoanBaroServiceDetailDoc from "./LoanBaroServiceDetailDoc";
 
 const BaroLaonWrap = styled.div`
   display: flex;
+  width: 100%;
 `;
 
-const BaroLoanTab = styled(Button)``;
+const BaroLoanTab = styled(Button)`
+  width: 49%;
+  height: 60px;
+  color: ${palette.gray[9]};
+  border-bottom: 1px solid ${palette.gray[4]};
+  background: url(${arrow_default_other}) no-repeat 50% 101%;
+  margin-bottom: 1em;
+  &:hover {
+    border-bottom: 2px solid ${palette.gray[6]};
+    background: url(${arrow_default_other}) no-repeat 50% 101%;
+  }
+  &.active {
+    font-weight: border;
+    font-size: 1.2em;
+    border-bottom: 3px solid ${palette.gray[9]};
+    background: url(${arrow_select_other}) no-repeat 50% 101%;
+  }
+`;
 
 const BaroContent = styled.div`
   h2 {
@@ -24,11 +47,19 @@ const BaroContent = styled.div`
     font-size: 1.5em;
     margin-bottom: 1em;
   }
+  h3 {
+    background-image: url(${icon_blue});
+    background-repeat: no-repeat;
+    text-indent: 1em;
+    font-size: 1.2em;
+    margin-bottom: 1em;
+  }
   li {
     background-image: url(${icon_tri});
     background-repeat: no-repeat;
     text-indent: 1em;
     font-size: 1em;
+    margin-left: 1em;
     margin-bottom: 1em;
   }
 
@@ -60,6 +91,7 @@ const BaroContent = styled.div`
     color: ${palette.orange[6]};
   }
   table {
+    margin-bottom: 1em;
     width: 100%;
     th,
     td {
@@ -70,8 +102,33 @@ const BaroContent = styled.div`
       padding-bottom: 12px;
     }
     td {
-      padding-left: 12px;
-      padding-top: 12px;
+      padding: 12px;
+    }
+    tbody {
+      &.gray {
+        th {
+          display: table-cell;
+          vertical-align: middle;
+          align-items: center;
+          font-weight: bolder;
+          background-color: ${palette.gray[2]};
+        }
+        .lh_20 {
+          line-height: 20px;
+        }
+        td {
+          display: table-cell;
+          vertical-align: middle;
+          align-items: center;
+          padding: 0;
+          text-align: center;
+        }
+        .operationTime {
+          display: inline-block;
+          width: 100%;
+          border-top: 1px dotted ${palette.gray[5]};
+        }
+      }
     }
   }
 `;
@@ -115,6 +172,9 @@ export default function BaroLoan() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const toggleSwitchHandler = (e) => {
+    setSecValue(Number(e.target.value));
+  };
 
   return (
     <MainContainer>
@@ -131,137 +191,33 @@ export default function BaroLoan() {
         </Box>
         <TabPanel value={value} index={0}>
           <BaroContent>
-            <h2>『희망도서 서비스』 란? </h2>
-            <ul>
-              <li>
-                수원시도서관에 원하는 자료가 없을 경우, 이용자가 원하는 자료를
-                신청할 수 있는 서비스
-              </li>
-              <li>
-                신청기간 : 희망도서 바로대출서비스 : 2022년 1월 3일(월) ~ 2022년
-                10월 31일(월)
-                <p className='textIndent6'>
-                  도서관 희망도서 서비스 : 2022년 1월 3일(월) ~ 2022년 10월
-                  31일(월)
-                </p>
-              </li>
-              <li>신청대상 : 수원시 도서관 정회원</li>
-              <li> 신청권수 : 월 1인 2권</li>
-              <li>
-                신청방법 : 도서관 또는 서점 중 대출하고 싶은 곳을 선택하여 신청
-                <p className='textIndent2'>
-                  <span className='blueText'>
-                    - 도서관 희망도서 서비스 (도서관 대출)
-                  </span>
-                </p>
-                <p className='textIndent2'>
-                  : 희망도서 신청 ⇒ 도서관에서 구입·등록 ⇒
-                  <span className='orangeText semiBorder'>
-                    도서관에서 대출/반납
-                  </span>
-                </p>
-                <p className='textIndent2'>
-                  <span className='blueText'>
-                    - 희망도서 바로대출 서비스 (서점 대출)
-                  </span>
-                </p>
-                <p className='textIndent2'>
-                  : 희망도서 신청 ⇒ 승인 ⇒{" "}
-                  <span className='orangeText semiBorder'>
-                    서점대출 ⇒ 서점반납{" "}
-                  </span>
-                  ⇒ 도서관 구입·등록
-                </p>
-              </li>
-              ※ 도서관 희망도서 서비스: 도서관에서 구입 후 대출(12일~18일
-              소요됨)
-              <p>
-                희망도서 바로대출 서비스: 서점 소장시 바로 대출(빠른 대출 필요시
-                신청)
-              </p>
-            </ul>
-            <table>
-              <thead>
-                <tr>
-                  <th colSpan='2'>희망도서 서비스</th>
-                </tr>
-                <tr>
-                  <th>도서관 희망도서 서비스</th>
-                  <th>희망도서 바로대출 서비스</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <p className='border'>
-                      · SMS(알림톡 또는 문자) 수신 후 도서관에서 대출
-                    </p>
-                    <p>{`<신청제외도서>`}</p>
-                    <p>- 각 도서관에서 이미 소장중인 경우</p>
-                    <p className='blueText3'>
-                      ※ 수원시도서관 상호대차 서비스로 대출가능도서(단,
-                      어린이도서 제외)
-                    </p>
-                    <p>
-                      - 발행년 <span className='orangeText'>5년 이상 도서</span>
-                    </p>
-                    <p>
-                      - <span className='orangeText'>5만원 초과 도서</span>
-                    </p>
-                    <p>
-                      <span className='orangeText'>
-                        기타 신청 제외 도서는 상세이용안내 참조
-                      </span>
-                    </p>
-                    <p className='blueText border'>
-                      · 대출기간 : 14일(연장가능)
-                    </p>
-                  </td>
-                  <td>
-                    <p className='border'>
-                      · SMS(알림톡 또는 문자) 수신 후 서점에서 바로 대출
-                    </p>
-                    <p>{`<신청제외도서>`}</p>
-                    <p>- 각 도서관에서 이미 소장중인 경우 </p>
-                    <p className='blueText3'>
-                      ※ 수원시도서관 상호대차 서비스로 대출가능도서(단,
-                      어린이도서 제외)
-                    </p>
-                    <p>
-                      - 발행년 <span className='orangeText'>1년 이상 </span>도서
-                    </p>
-                    <p>
-                      - <span className='orangeText'> 3만원 초과 </span>도서
-                    </p>
-
-                    <p>
-                      -{" "}
-                      <span className='orangeText'>
-                        기타 신청 제외 도서는 상세이용안내 참조
-                      </span>
-                    </p>
-                    <p className='blueText border'>
-                      · 대출기간 : 14일(연장불가)
-                    </p>
-                    <p className='blueText3 textIndent2'>
-                      ※ 미대출시 : 다음달 희망도서 바로대출 서비스 신청 제한
-                    </p>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <BaroLoanServiceDoc />
           </BaroContent>
         </TabPanel>
         <TabPanel value={value} index={1}>
           <BaroLaonWrap>
-            <BaroLoanTab secValue={secValue}>
+            <BaroLoanTab
+              onClick={toggleSwitchHandler}
+              value='0'
+              className={`${secValue === 0 ? "active" : ""}`}
+            >
               도서관 희망도서 서비스
             </BaroLoanTab>
-            <BaroLoanTab secValue={secValue}>
+            <BaroLoanTab
+              onClick={toggleSwitchHandler}
+              value='1'
+              className={`${secValue === 1 ? "active" : ""}`}
+            >
               희망도서 바로대출 서비스
             </BaroLoanTab>
           </BaroLaonWrap>
-          <BaroContent></BaroContent>
+          <BaroContent>
+            {secValue === 0 ? (
+              <BaroLoanServiceDetailDoc />
+            ) : (
+              <LoanBaroServiceDetailDoc />
+            )}
+          </BaroContent>
         </TabPanel>
       </Box>
     </MainContainer>
