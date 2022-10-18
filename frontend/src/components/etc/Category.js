89f030edf79604644ec26c4eac682164a14e4840
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-10-18 17:51:22 +0900

    (feat) 카테고리 분류 위치 체크

---
+++ frontend/src/components/etc/Category.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainContainer from "../common/MainContainer";
+import TestBox from "../common/TestBox";
+function Category() {
+  return (
+    <MainContainer>
+      <TestBox></TestBox>
+    </MainContainer>
+  );
+}
+
+export default Category;
