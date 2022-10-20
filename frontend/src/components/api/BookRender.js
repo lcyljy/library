65b9fe784a6e6c5eb86eda51c3e0cdeeac48e4e4
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-10-21 00:43:22 +0900

    (feat) 책 클릭시 개별페이지로 이동 구현
    #29

--- frontend/src/components/api/BookRender.js
+++ frontend/src/components/api/BookRender.js
@@ -17,7 +17,7 @@ import DateFilter, {
   startDt,
   endDt,
 } from "../fillter/DateFilter";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const API_KEY = process.env.REACT_APP_DATA4LIBRARY_KEY;
 
@@ -267,21 +267,26 @@ function BookRender(props) {
             })
             .map((DB, i) => {
               return (
-                <li key={`${DB.isbn13} ${DB.vol} ${i + 1}`}>
-                  <h2>{i + 1}</h2>
-                  <BookImage
-                    style={{
-                      backgroundImage: `url(${
-                        DB.bookImageURL ? DB.bookImageURL : no_image
-                      })`,
-                    }}
-                  ></BookImage>
+                <Link
+                  to={`/singleBook/${DB.isbn13}`}
+                  key={`${DB.isbn13} ${DB.vol} ${i + 1}`}
+                >
+                  <li>
+                    <h2>{i + 1}</h2>
+                    <BookImage
+                      style={{
+                        backgroundImage: `url(${
+                          DB.bookImageURL ? DB.bookImageURL : no_image
+                        })`,
+                      }}
+                    ></BookImage>
 
-                  <p>
-                    {DB.bookname} {DB.vol ? `= ${DB.vol}` : null}
-                  </p>
-                  <p>{DB.authors}</p>
-                </li>
+                    <p>
+                      {DB.bookname} {DB.vol ? `= ${DB.vol}` : null}
+                    </p>
+                    <p>{DB.authors}</p>
+                  </li>
+                </Link>
               );
             })
             .slice(offSet, offSet + limit)}
