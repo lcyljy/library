49f17ae8b9c65a9cd17d83fe532298e658ab0ecb
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-10-26 11:53:24 +0900

    (feat) 경로 재설정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
